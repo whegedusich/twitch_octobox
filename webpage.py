bbd164c4e2e068fceb48bdcce24d57d0ceab08a4
William Hegedusich <hegedusich@pm.me>
2021-02-15 00:26:33 -0600

    Added needed files
    Necessary files for basic functionality. Need to improve dynamic scaling based on number of streams.

---
+++ webpage.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, url_for
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template('Main.html')
+
+
+@app.route("/Streams", methods=["POST", "GET"])
+def streams():
+    if request.method == "POST":
+        streamers = request.form
+
+        return render_template('Streams.html', chans=streamers)
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port='80')
